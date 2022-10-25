a7fc65cba0b9feb1526e51a847a925116a1924eb
nikolaiMurga <nicolai.murga@gmail.com>
2022-10-25 20:15:57 +0200

    [WIP] added splash widget.

--- lib/presentation/splash/bloc/splash_state.dart
+++ lib/presentation/splash/bloc/splash_state.dart
@@ -1,6 +1,8 @@
 part of 'splash_cubit.dart';
 
-@immutable
-abstract class SplashState {}
+abstract class SplashState extends Equatable{}
 
-class SplashInitial extends SplashState {}
+class SplashInitial extends SplashState {
+  @override
+  List<Object?> get props => [];
+}
--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,10 +1,14 @@
+import 'package:beerville_cubit/app/locator.dart';
+import 'package:beerville_cubit/presentation/splash/bloc/splash_cubit.dart';
+import 'package:beerville_cubit/presentation/splash/splash_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(create: (_) => locator<SplashCubit>(), child: SplashWidget());
   }
 }
